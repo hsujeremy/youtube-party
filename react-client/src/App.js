59d12a71508add6a393c125b124b96734d238fc6
Jeremy Hsu <jeremyhsu1@gmail.com>
2021-03-22 18:21:26 -0400

    Clean up App file

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -14,54 +14,35 @@ function Video(props) {
 
     React.useEffect(() => {
         if (event) {
-            // if (props.isPaused) {
-            //     event.target.pauseVideo();
-            // } else {
-            //     event.target.playVideo();
-            // }
-            console.log(event.target.getPlayerState());
-            const payload = {
+            props.socket.emit('message', {
                 'action': 'playpause',
                 'state': event.target.getPlayerState()
-            };
-            props.socket.emit('message', payload);
+            });
         }
     }, [props.playPauseCounter]);
 
     React.useEffect(() => {
         if (event) {
             event.target.pauseVideo();
-            const payload = {
+            props.socket.emit('message', {
                 'action': 'sync',
                 'timestamp': event.target.getCurrentTime()
-            };
-            props.socket.emit('message', payload);
+            });
         }
     }, [props.syncCounter]);
 
-    React.useEffect(() => {
-        console.log('called effect')
-        if (event) {
-            event.target.playVideo();
-        }
-    }, [props.playCounter]);
-
-    React.useEffect(() => {
-        if (event) {
-            event.target.pauseVideo();
-        }
-    }, [props.pauseCounter]);
-
     props.socket.on('message', (message) => {
-        if (message.action === 'sync' && event) {
-            event.target.seekTo(message.timestamp);
-            event.target.pauseVideo();
-            event.target.playVideo();
-        } else if (message.action === 'playpause' && event) {
-            if (message.state === 1) {
+        if (event) {
+            if (message.action === 'sync') {
+                event.target.seekTo(message.timestamp);
                 event.target.pauseVideo();
-            } else {
                 event.target.playVideo();
+            } else if (message.action === 'playpause') {
+                if (message.state === 1) {
+                    event.target.pauseVideo();
+                } else {
+                    event.target.playVideo();
+                }
             }
         }
     });
@@ -76,7 +57,7 @@ function Video(props) {
 
     return (
         <YouTube
-            videoId={'1BfCnjr_Vjg'}
+            videoId={props.videoId}
             opts={opts}
             onReady={_onReady}
             onStateChange={_onStateChange}
@@ -84,33 +65,18 @@ function Video(props) {
     );
 }
 
-function Content() {
+function App() {
     const [videoUrl, setVideoUrl] = React.useState('');
 
-    const [isPaused, setIsPaused] = React.useState(false);
     const [playPauseCounter, incrementPlayPauseCounter] = React.useState(0);
     const [syncCounter, incrementSyncCounter] = React.useState(0);
 
-    const [playCounter, incrementPlayCounter] = React.useState(0);
-    const [pauseCounter, incrementPauseCounter] = React.useState(0);
-
     const togglePause = () => {
-        socket.emit('message', 'From togglePause in client')
-        setIsPaused(!isPaused);
         incrementPlayPauseCounter(playPauseCounter + 1);
     };
 
     const sync = () => {
-        socket.emit('message', 'Sync!');
         incrementSyncCounter(syncCounter + 1);
-    };
-
-    const playVideo = () => {
-        incrementPlayCounter(playCounter + 1);
-    };
-
-    const pauseVideo = () => {
-        incrementPauseCounter(pauseCounter + 1);
     };
 
     const socket = socketClient('http://127.0.0.1:8080');
@@ -129,7 +95,13 @@ function Content() {
             <div>
                 <input value={videoUrl} onChange={(e) => setVideoUrl(e.target.value)} />
 
-                <Video socket={socket} isPaused={isPaused} playPauseCounter={playPauseCounter} syncCounter={syncCounter} playCounter={playCounter} pauseCounter={pauseCounter} />
+                <Video
+                    videoId={'1BfCnjr_Vjg'}
+                    socket={socket}
+                    playPauseCounter={playPauseCounter}
+                    syncCounter={syncCounter}
+                />
+
                 <button onClick={togglePause}>Play/Pause</button>
 
                 <button onClick={sync}>Sync</button>
@@ -138,4 +110,4 @@ function Content() {
     )
 }
 
-export default Content;
+export default App;
