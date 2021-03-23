646dc96765211523b369fd2fac365d8ddd86e031
Jeremy Hsu <jeremyhsu1@gmail.com>
2021-03-23 11:48:00 -0400

    Add express app

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const http = require('http').createServer();
+const app = require('express')();
+const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
     cors: { origin: '*' }
@@ -11,6 +12,12 @@ io.on('connection', (socket) => {
     socket.on('message', (message) => {
         io.emit('message', message);
     });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
-http.listen(8080, () => console.log('Server listening on http://localhost:8080'));
+http.listen(8080, () => {
+    console.log('Server listening on http://localhost:8080')
+});
