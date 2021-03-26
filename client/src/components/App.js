0f90285a5ad083dd1c79cdd2cb3bb21b35da28e7
Jeremy Hsu <jeremyhsu1@gmail.com>
2021-03-26 11:37:20 -0400

    Move user count indicator to separate component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import SearchHead from './SearchHead.js';
 import Video from './Video.js';
+import UserCount from './UserCount';
 import { socket } from '../services/socket';
 
 
 const App = () => {
     const [videoUrl, setVideoUrl] = React.useState('');
-
     const [playPauseCounter, incrementPlayPauseCounter] = React.useState(0);
     const [syncCounter, incrementSyncCounter] = React.useState(0);
-
-    const [numUsers, updateNumUsers] = React.useState(0);
-
-    React.useEffect(() => {
-        socket.on('connection', (content) => {
-            updateNumUsers(content.numUsers);
-        });
-
-        socket.on('disconnection', (content) => {
-            updateNumUsers(content.numUsers);
-        });
-    });
 
     const togglePause = () => {
         incrementPlayPauseCounter(playPauseCounter + 1);
@@ -38,6 +26,7 @@ const App = () => {
             return (
                 <div>
                     <SearchHead videoUrl={videoUrl} onVideoUrlChange={(url) => setVideoUrl(url)} />
+                    <UserCount />
                     <p>Please enter a valid URL</p>
                 </div>
             );
@@ -46,21 +35,22 @@ const App = () => {
         return (
             <div>
                 <SearchHead videoUrl={videoUrl} onVideoUrlChange={(url) => setVideoUrl(url)} />
-                    <Video
-                        videoId={videoCode}
-                        playPauseCounter={playPauseCounter}
-                        syncCounter={syncCounter}
-                    />
+                <UserCount />
+                <Video
+                    videoId={videoCode}
+                    playPauseCounter={playPauseCounter}
+                    syncCounter={syncCounter}
+                />
 
-                    <button onClick={togglePause}>Play/Pause</button>
-                    <button onClick={sync}>Sync</button>
+                <button onClick={togglePause}>Play/Pause</button>
+                <button onClick={sync}>Sync</button>
             </div>
         );
     }
     return (
         <div>
             <SearchHead videoUrl={videoUrl} onVideoUrlChange={(url) => setVideoUrl(url)} />
-            <div>{numUsers}</div>
+            <UserCount />
         </div>
     );
 };
---
+++ client/src/components/UserCount.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { socket } from '../services/socket';
+
+
+const UserCount = () => {
+    const [numUsers, updateNumUsers] = React.useState(0);
+
+    React.useEffect(() => {
+        socket.on('connection', (content) => {
+            updateNumUsers(content.numUsers);
+        });
+
+        socket.on('disconnection', (content) => {
+            updateNumUsers(content.numUsers);
+        });
+    });
+
+    if (numUsers === 1) {
+        return <div>Currently Online: {numUsers} user</div>
+    }
+    return <div>Currently Online: {numUsers} users</div>;
+};
+
+export default UserCount;
